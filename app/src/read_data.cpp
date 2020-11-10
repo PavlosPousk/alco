107bfb41c633ff5d8934dda17a8d0df3283d4c29
PavlosPousk <73303840+PavlosPousk@users.noreply.github.com>
2020-11-10 17:48:10 +0200

    new

--- app/src/read_data.cpp
+++ app/src/read_data.cpp
@@ -6,6 +6,23 @@
 
 using namespace std;
 
+int count_common_elements(set<int> s1, set<int> s2)
+{
+    int c = 0;
+    for (int x : s1)
+    {
+        for (int y : s2)
+        {
+            if (x == y)
+            {
+                c++;
+                break;
+            }
+        }
+    }
+    return c;
+}
+
 void read_data(string fn, int students, int exams)
 {
     vector<set<int>> exam_students(exams + 1);
@@ -54,13 +71,39 @@ void read_data(string fn, int students, int exams)
         }
     }
 
-    for (int i =1; i<exams; i++){
-        cout << i<<": ";
-        for (int j=1; j<exams; j++){
-            cout << AdjMatrix[i][j];
-        } 
-        cout << endl;
+    cout << "Adjacency Matrix" << endl;
+    int *adj_matrix = new int[exams * exams];
+    for (int i = 0; i < exams; i++)
+    {
+        for (int j = 0; j < exams; j++)
+        {
+            if (i == j){
+                adj_matrix[i * exams + j] = 0; // adj_matrix[i][j]
+                continue;
+            }
+            int c = count_common_elements(exam_students[i + 1], exam_students[j + 1]);
+            if (c > 0)
+                cout << i + 1 << " " << j + 1 << " " << c << endl;
+            adj_matrix[i * exams + j] = c;
+        }
     }
+
+    int c = 0;
+    for (int i = 0; i < exams; i++)
+    {
+        for (int j = 0; j < exams; j++)
+        {
+            if (adj_matrix[i * exams + j] > 0)
+            {
+                c++;
+            }
+        }
+    }
+
+    double cd = double(c) / double(exams * exams);
+    cout << "Conflict Density: " << cd << endl;
+
+    delete[] adj_matrix;
 }
 
 int main()
