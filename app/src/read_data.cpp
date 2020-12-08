788058fe29d943cda80d141c449740975e0a6590
PavlosPousk <73303840+PavlosPousk@users.noreply.github.com>
2020-12-08 18:37:59 +0200

    new

--- app/src/read_data.cpp
+++ app/src/read_data.cpp
@@ -85,8 +85,8 @@ void read_data(string fn, int students, int exams)
                 continue;
             }
             int c = count_common_elements(exam_students[i + 1], exam_students[j + 1]);
-            if (c > 0){ s=s+1;
-                cout << s << ": " << i + 1 << " " << j + 1 << " " << c << endl;}   
+            if (c > 0) 
+                cout << i + 1 << " " << j + 1 << " " << c << endl;   
             adj_matrix[i * exams + j] = c;
             }
     } 
